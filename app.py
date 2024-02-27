8bd42cf8624e250897fb5fd42c55383b041fc25f
Eseigbe111 <tempeseigbe@gmail.com>
2024-02-27 11:15:28 +0100

    new

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def allusers():
     allusers_fetched = db.execute('select * from users')
     allusers = allusers_fetched.fetchall()
 
-    return render_template('allusers.html', user=user, allusers = alluser)
+    return render_template('allusers.html', user=user, allusers = allusers)
 
 @app.route('/addTeacher/<int:id>', methods=["POST", "GET"])
 def addTeacher(id):
